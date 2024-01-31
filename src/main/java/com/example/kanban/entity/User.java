6e969542892f22ca71882f76b4383f5646771f98
Sergey Malykh <sergeymalykhdm@gmail.com>
2024-01-31 18:05:37 +0600

    Add JsonProperty for password setter.

--- src/main/java/com/example/kanban/entity/User.java
+++ src/main/java/com/example/kanban/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.kanban.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -84,6 +85,7 @@ public class User implements UserDetails, Authentication {
         return password;
     }
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public void setPassword(String password) {
         this.password = password;
     }
